9f34013238f1faf43cd8c322a42cc6ba746fd885
luisapcc <00001078481751sp@al.educacao.sp.gov.br>
2024-11-21 14:36:10 -0300

    Add files via upload

---
+++ flashcards/perguntas.js
@@ -0,0 +1,143 @@
+criaCartao(
+    'Jogos',
+    '"Acabe com ele!" é a frase icônica de que série de luta brutal?',
+    'Mortal Kombat'
+)
+
+criaCartao(
+    'Jogos',
+    'Em qual jogo de terror espacial os jogadores escapam de um perigoso Xenomorfo?',
+    'Alienígena: Isolamento'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual franquia de RPG pós-apocalíptico se passa em Wasteland?',
+    'precipitação'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de terror desafia os jogadores com animais animatrônicos sobreviventes?',
+    'Cinco noites no Freddy'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de construção permite que os jogadores provoquem desastres naturais nas cidades?',
+    'Sim City'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo MMORPG possui milhões de assinantes mensais ativos?',
+    'World of Warcraft'
+)
+
+criaCartao(
+    'Jogos',
+    'A série de terror de sobrevivência da Capcom, estrelada por Chris Redfield, concentra-se em quais armas biológicas?',
+    'Resident Evil'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual herói da Sony é extremamente protetor com a figura de sua filha adotiva?',
+    'Kratos (Deus da Guerra)'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual atirador de 2007 foi ridicularizado criticamente por causa da polêmica missão do “aeroporto”?',
+    'Call of Duty: Modern Warfare 2'
+)
+
+criaCartao(
+    'Jogos',
+    '“Um homem escolhe, um escravo obedece” é uma frase-chave da filosofia de qual vilão de videogame?',
+    'Andrew Ryan (Bioshock)'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o enredo principal de Silent Hill 2?',
+    'O protagonista James Sunderland viaja para Silent Hill após receber uma carta de sua falecida esposa Mary, onde descobre a verdade sobre sua morte e seus próprios traumas'
+)
+
+criaCartao(
+    'Jogos',
+    'Quem é o vilão principal em Resident Evil Village?',
+    'Mão Miranda, que manipula os eventos do jogo e está por trás do sequestro da filha de Ethan, Rose'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de terror popular se passa em uma casa infestada por fantasmas e usa uma câmera como arma principal?',
+    'Fatal Frame'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o monstro mais icônico da franquia Dead Space?',
+    'Necromorphs, criaturas mutantes criadas por um sinal alienígena'
+)
+
+criaCartao(
+    'Jogos',
+    'Em Outlast, qual é o nome do asilo onde o jogo se passa?',
+    'Mount Massive Asylum'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de terror é conhecido por seu sistema de escolhas moral e múltiplos finais, incluindo o final "Dog Ending"?',
+    'Silesnt Hill 2'
+)
+
+criaCartao(
+    'Jogos',
+    'Que jogo de terror indie coloca o jogador em um hospital abandonado enquanto tenta escapar de um monstro chamado "The Groom"?',
+    'Outlast: Whistleblower'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo tem como protagonista uma jornalista chamada Elena que investiga rituais ocultos?',
+    'Song of Horror'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de terror usa gráficos retrô e combina elementos de investigação policial e horror cósmico?',
+    'world of horror'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual é o principal objetivo no jogo Amnesia: The Dark Descent?',
+    'O jogador controla Daniel, que precisa recuperar sua memória perdida enquanto enfrenta monstros e resolve enigmas em um castelo assombrado'
+)
+
+criaCartao(
+    'Jogos',
+    'Em The Evil Within, qual é o nome do detetive protagonista?',
+    'Sebastian Castellanos'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual jogo de terror é ambientado em um navio cruzeiro abandonado?',
+    'Man of Medan'
+)
+
+criaCartao(
+    'Jogos',
+    'Que jogo de terror famoso foi inspirado por lendas urbanas japonesas e histórias de fantasmas?',
+    'Fatal Frame'
+)
+
+criaCartao(
+    'Jogos',
+    'Qual franquia de terror apresenta cenários pós-apocalípticos e criaturas chamadas "Clickers"?',
+    'The Last of Us'
+)
